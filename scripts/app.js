c4ca30816e7a35d79406473e38768aa99d5d5fd3
Heather Voyles <voylesh@gmail.com>
2015-04-19 19:07:28 -0300

    Eighth Commit Refactoring Browser

--- scripts/app.js
+++ scripts/app.js
@@ -6,13 +6,15 @@ var app = angular
     'ngResource',    
     'ngRoute',    
     'firebase',
-    'toaster'
+    'toaster', 
+    'angularMoment'
   ])
   .constant('FURL', 'https://task-guru.firebaseio.com/')  
   .config(function ($routeProvider) {
     $routeProvider      
       .when('/', {
-        templateUrl: 'views/browse.html'        
+        templateUrl: 'views/browse.html',   
+        controller: 'BrowseController'   
       })
       .when('/login', { 
           templateUrl: 'views/login.html',
@@ -30,9 +32,9 @@ var app = angular
           //templateUrl: 'views/edit.html',
           //controller: 'TaskController'
         //})
-      .when('/browse', { 
+      .when('/browse/:taskId', { 
           templateUrl: 'views/browse.html',
-          controller: 'TaskController'
+          controller: 'BrowseController'
         })
       .otherwise({
         redirectTo: '/'
